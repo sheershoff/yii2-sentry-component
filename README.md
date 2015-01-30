11ab1077996e9e890cce8d46c0ec3f980fb4cca1
Ilya Sheershoff <sheershoff@gmail.com>
2015-01-30 12:46:17 +0500

    v 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# yii2-sentry-component
+Yii2 sentry component
+=====================
+
 Yii2 sentry component allowing for unified array format passing parameters to sentry log target and other log targets
+
+##Install
+```
+php composer.phar require e96/yii2-sentry:dev-master
+```
+
+## Usage
+In config file:
+
+```php
+'bootstrap' => ['log', 'raven'],
+'components' => [
+    'raven' => [
+        'class' => 'e96\sentry\ErrorHandler',
+        'dsn' => '', // Sentry DSN
+    ],
+    'log' => [
+        'targets' => [
+            [
+                'class' => 'e96\sentry\Target',
+                'levels' => ['error', 'warning'],
+                'dsn' => '', // Sentry DSN
+            ]
+        ],
+    ],
+]
+```
+You can provide additional information with exceptions:
+```php
+/** @var ErrorHandler $raven */
+$raven = \Yii::$app->get('raven');
+$raven->client->extra_context($task->attributes);
+
+throw new Exception('unknown task type');
+```
+
+## Rich logging through built in Yii2 logging methods
+
+```php
+try{
+    $model1->save();
+}catch (\Exception $e){
+    Yii::warning([
+        'msg' => 'MsrpAddExceptionSentryTest', // this is for the text msg
+        'data' => [ // additional data sent as 'extra_data'
+            'userId' => Yii::$app->user->id,
+            'action' => 'MsrpAddExceptionSentryTest',
+        ],
+        'exception' => $e->getTrace(), // to get the native Sentry stacktrace feature available
+    ], 'commerceMsrp');
+}
+```
---
+++ src/ErrorHandler.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace sheershoff\sentry;
+
+use yii\base\Component;
+
+/**
+ * @property \Raven_Client $client
+ */
+class ErrorHandler extends Component
+{
+    /**
+     * @var string Sentry DSN
+     */
+    public $dsn;
+
+    /**
+     * @var array Raven client options.
+     * @see \Raven_Client::__construct for more details
+     */
+    public $clientOptions = [];
+
+    /**
+     * @var \Raven_Client
+     */
+    protected $client;
+
+    /**
+     * @var \Raven_ErrorHandler
+     */
+    protected $ravenErrorHandler;
+
+    /**
+     * @var callable|null
+     */
+    protected $oldExceptionHandler;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->client = new \Raven_Client($this->dsn, $this->clientOptions);
+
+        $this->ravenErrorHandler = new \Raven_ErrorHandler($this->client);
+        $this->ravenErrorHandler->registerErrorHandler(true);
+        // shutdown function not working in yii2 yet: https://github.com/yiisoft/yii2/issues/6637
+        //$this->ravenErrorHandler->registerShutdownFunction();
+        $this->oldExceptionHandler = set_exception_handler(array($this, 'handleYiiExceptions'));
+    }
+
+    public function handleYiiExceptions($e)
+    {
+        if (!$e instanceof \yii\base\ErrorException) {
+            $e->event_id = $this->client->getIdent($this->client->captureException($e));
+        }
+
+        if ($this->oldExceptionHandler) {
+            call_user_func($this->oldExceptionHandler, $e);
+        }
+    }
+
+    /**
+     * @return \Raven_Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+}
